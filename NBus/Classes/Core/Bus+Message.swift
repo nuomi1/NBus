ef22532e7b3b3499789cbf8d0af3803e2f485474
nuomi1 <nuomi1@qq.com>
2020-08-23 21:05:50 +0800

    feat: Message

---
+++ NBus/Classes/Core/Bus+Message.swift
@@ -0,0 +1,18 @@
+//
+//  Bus+Message.swift
+//  NBus
+//
+//  Created by nuomi1 on 2020/8/23.
+//  Copyright © 2020 nuomi1. All rights reserved.
+//
+
+public struct Message: RawRepresentable, Hashable {
+
+    public typealias RawValue = String
+
+    public let rawValue: Self.RawValue
+
+    public init(rawValue: Self.RawValue) {
+        self.rawValue = rawValue
+    }
+}
