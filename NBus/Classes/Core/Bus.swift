a994a878e63e9b85c6844673400bedd4f33a7ca6
nuomi1 <nuomi1@qq.com>
2020-08-23 21:01:10 +0800

    feat: Bus

---
+++ NBus/Classes/Core/Bus.swift
@@ -0,0 +1,12 @@
+//
+//  Bus.swift
+//  NBus
+//
+//  Created by nuomi1 on 2020/8/23.
+//  Copyright © 2020 nuomi1. All rights reserved.
+//
+
+public final class Bus {
+
+    public static let shared = Bus()
+}
