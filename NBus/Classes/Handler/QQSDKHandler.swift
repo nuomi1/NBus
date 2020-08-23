8827fd00bb8e66709ed10a5e0de94ca26457f7c4
nuomi1 <nuomi1@qq.com>
2020-08-23 21:44:04 +0800

    feat: QQSDKHandler

---
+++ NBus/Classes/Handler/QQSDKHandler.swift
@@ -0,0 +1,11 @@
+//
+//  QQSDKHandler.swift
+//  NBus
+//
+//  Created by nuomi1 on 2020/8/23.
+//  Copyright © 2020 nuomi1. All rights reserved.
+//
+
+import Foundation
+
+public class QQSDKHandler {}
