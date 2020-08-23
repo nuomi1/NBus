ff99317a67fa1bac14b7c53b9ac94b4c755681d0
nuomi1 <nuomi1@qq.com>
2020-08-23 21:11:04 +0800

    feat: Error

---
+++ NBus/Classes/Core/Bus+Error.swift
@@ -0,0 +1,12 @@
+//
+//  Bus+Error.swift
+//  NBus
+//
+//  Created by nuomi1 on 2020/8/23.
+//  Copyright © 2020 nuomi1. All rights reserved.
+//
+
+extension Bus {
+
+    public enum Error: Swift.Error {}
+}
