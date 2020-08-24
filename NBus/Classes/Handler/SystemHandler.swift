d6703641114c18a7660304e74c720b00d770bcfb
nuomi1 <nuomi1@qq.com>
2020-08-24 21:08:51 +0800

    feat: SystemHandler

---
+++ NBus/Classes/Handler/SystemHandler.swift
@@ -0,0 +1,11 @@
+//
+//  SystemHandler.swift
+//  NBus
+//
+//  Created by nuomi1 on 2020/8/24.
+//  Copyright © 2020 nuomi1. All rights reserved.
+//
+
+import Foundation
+
+public class SystemHandler {}
