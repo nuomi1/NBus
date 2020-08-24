35629b30af14f86d8e5e9e1489c837ba789743df
nuomi1 <nuomi1@qq.com>
2020-08-24 20:28:50 +0800

    feat: WechatSDKHandler implement ShareHandlerType

--- NBus/Classes/Handler/WechatSDKHandler.swift
+++ NBus/Classes/Handler/WechatSDKHandler.swift
@@ -8,4 +8,154 @@
 
 import Foundation
 
-public class WechatSDKHandler {}
+public class WechatSDKHandler {
+
+    public let endpoints: [Endpoint] = [
+        Endpoints.Wechat.friend,
+        Endpoints.Wechat.timeline,
+        Endpoints.Wechat.favorite,
+    ]
+
+    public var isInstalled: Bool {
+        WXApi.isWXAppInstalled()
+    }
+
+    private var shareCompletionHandler: Bus.ShareCompletionHandler?
+}
+
+extension WechatSDKHandler: ShareHandlerType {
+
+    // swiftlint:disable cyclomatic_complexity function_body_length
+
+    public func share(
+        message: MessageType,
+        to endpoint: Endpoint,
+        options: [Bus.ShareOptionKey: Any] = [:],
+        completionHandler: @escaping Bus.ShareCompletionHandler
+    ) {
+        guard isInstalled else {
+            completionHandler(.failure(.missingApplication))
+            return
+        }
+
+        guard canShare(message: message.identifier, to: endpoint) else {
+            completionHandler(.failure(.unsupportedMessage))
+            return
+        }
+
+        shareCompletionHandler = completionHandler
+
+        let request = SendMessageToWXReq()
+        request.scene = Int32(scene(endpoint).rawValue)
+
+        let mediaMessage = WXMediaMessage()
+
+        if let message = message as? MediaMessageType {
+            mediaMessage.title = message.title ?? ""
+            mediaMessage.description = message.description ?? ""
+            mediaMessage.thumbData = message.thumbnail
+        }
+
+        switch message {
+        case let message as TextMessage:
+            request.text = message.text
+            request.bText = true
+
+        case let message as ImageMessage:
+            let imageObject = WXImageObject()
+            imageObject.imageData = message.data
+
+            mediaMessage.mediaObject = imageObject
+
+        case let message as AudioMessage:
+            let audioObject = WXMusicObject()
+            audioObject.musicUrl = message.link.absoluteString
+            audioObject.musicDataUrl = message.dataLink?.absoluteString ?? ""
+
+            mediaMessage.mediaObject = audioObject
+
+        case let message as VideoMessage:
+            let videoObject = WXVideoObject()
+            videoObject.videoUrl = message.link.absoluteString
+
+            mediaMessage.mediaObject = videoObject
+
+        case let message as WebPageMessage:
+            let webPageObject = WXWebpageObject()
+            webPageObject.webpageUrl = message.link.absoluteString
+
+            mediaMessage.mediaObject = webPageObject
+
+        case let message as FileMessage:
+            let fileObject = WXFileObject()
+            fileObject.fileData = message.data
+            fileObject.fileExtension = message.fileExtension
+
+            mediaMessage.mediaObject = fileObject
+
+        case let message as MiniProgramMessage:
+            let miniProgramObject = WXMiniProgramObject()
+            miniProgramObject.webpageUrl = message.link.absoluteString
+            miniProgramObject.userName = message.miniProgramID
+            miniProgramObject.path = message.path
+            miniProgramObject.miniProgramType = miniProgramType(message.miniProgramType)
+            miniProgramObject.hdImageData = message.thumbnail
+
+            mediaMessage.mediaObject = miniProgramObject
+
+        default:
+            assertionFailure()
+            completionHandler(.failure(.unsupportedMessage))
+            return
+        }
+
+        request.message = mediaMessage
+
+        WXApi.send(request) { result in
+            if !result {
+                completionHandler(.failure(.invalidMessage))
+            }
+        }
+    }
+
+    // swiftlint:enable cyclomatic_complexity function_body_length
+
+    private func canShare(message: Message, to endpoint: Endpoint) -> Bool {
+        switch endpoint {
+        case Endpoints.Wechat.friend:
+            return true
+        case Endpoints.Wechat.timeline:
+            return ![Messages.file, Messages.miniProgram].contains(message)
+        case Endpoints.Wechat.favorite:
+            return ![Messages.miniProgram].contains(message)
+        default:
+            assertionFailure()
+            return false
+        }
+    }
+
+    private func scene(_ endpoint: Endpoint) -> WXScene {
+        switch endpoint {
+        case Endpoints.Wechat.friend:
+            return WXSceneSession
+        case Endpoints.Wechat.timeline:
+            return WXSceneTimeline
+        case Endpoints.Wechat.favorite:
+            return WXSceneFavorite
+        default:
+            assertionFailure()
+            return WXSceneSession
+        }
+    }
+
+    private func miniProgramType(_ miniProgramType: MiniProgramMessage.MiniProgramType) -> WXMiniProgramType {
+        switch miniProgramType {
+        case .release:
+            return .release
+        case .test:
+            return .test
+        case .preview:
+            return .preview
+        }
+    }
+}
