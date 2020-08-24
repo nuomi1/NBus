c4b512e0c79eddca58c1cc3daee48d5360562fac
nuomi1 <nuomi1@qq.com>
2020-08-24 20:22:31 +0800

    feat: WechatSDKHandler

---
+++ NBus/Classes/Handler/WechatSDKHandler.swift
@@ -0,0 +1,11 @@
+//
+//  WechatSDKHandler.swift
+//  NBus
+//
+//  Created by nuomi1 on 2020/8/24.
+//  Copyright © 2020 nuomi1. All rights reserved.
+//
+
+import Foundation
+
+public class WechatSDKHandler {}
