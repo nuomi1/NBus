09292e2373e8eaec8ebca582bfa836ff2b3efd7e
nuomi1 <nuomi1@qq.com>
2020-08-24 20:52:08 +0800

    feat: WeiboSDKHandler implement ShareHandlerType

--- NBus/Classes/Handler/WeiboSDKHandler.swift
+++ NBus/Classes/Handler/WeiboSDKHandler.swift
@@ -8,4 +8,67 @@
 
 import Foundation
 
-public class WeiboSDKHandler {}
+public class WeiboSDKHandler {
+
+    public let endpoints: [Endpoint] = [
+        Endpoints.Weibo.timeline,
+    ]
+
+    public var isInstalled: Bool {
+        WeiboSDK.isWeiboAppInstalled()
+    }
+
+    private var shareCompletionHandler: Bus.ShareCompletionHandler?
+}
+
+extension WeiboSDKHandler: ShareHandlerType {
+
+    public func share(
+        message: MessageType,
+        to endpoint: Endpoint,
+        options: [Bus.ShareOptionKey: Any] = [:],
+        completionHandler: @escaping Bus.ShareCompletionHandler
+    ) {
+        guard isInstalled else {
+            completionHandler(.failure(.missingApplication))
+            return
+        }
+
+        shareCompletionHandler = completionHandler
+
+        let request = WBSendMessageToWeiboRequest()
+        request.message = WBMessageObject()
+
+        switch message {
+        case let message as TextMessage:
+            request.message.text = message.text
+
+        case let message as ImageMessage:
+            let imageObject = WBImageObject()
+            imageObject.imageData = message.data
+
+            request.message.imageObject = imageObject
+
+        case let message as WebPageMessage:
+            let webPageObject = WBWebpageObject()
+            webPageObject.webpageUrl = message.link.absoluteString
+            webPageObject.title = message.title
+            webPageObject.description = message.description
+            webPageObject.thumbnailData = message.thumbnail
+
+            webPageObject.objectID = UUID().uuidString
+
+            request.message.mediaObject = webPageObject
+
+        default:
+            completionHandler(.failure(.unsupportedMessage))
+            return
+        }
+
+        let result = WeiboSDK.send(request)
+
+        if !result {
+            completionHandler(.failure(.invalidMessage))
+        }
+    }
+}
