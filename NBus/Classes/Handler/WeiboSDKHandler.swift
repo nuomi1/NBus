a2849c2f508a11b9cadda7a158aa43d7974bcd5d
nuomi1 <nuomi1@qq.com>
2020-08-24 20:48:45 +0800

    feat: WeiboSDKHandler

---
+++ NBus/Classes/Handler/WeiboSDKHandler.swift
@@ -0,0 +1,11 @@
+//
+//  WeiboSDKHandler.swift
+//  NBus
+//
+//  Created by nuomi1 on 2020/8/24.
+//  Copyright © 2020 nuomi1. All rights reserved.
+//
+
+import Foundation
+
+public class WeiboSDKHandler {}
