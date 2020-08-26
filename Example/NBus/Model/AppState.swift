c679c075bcd1a171fa01a3cd4eccf4b18d5644ae
nuomi1 <nuomi1@qq.com>
2020-08-26 20:18:28 +0800

    feat: AppState

---
+++ Example/NBus/Model/AppState.swift
@@ -0,0 +1,16 @@
+//
+//  AppState.swift
+//  BusMock
+//
+//  Created by nuomi1 on 2020/8/26.
+//  Copyright © 2020 nuomi1. All rights reserved.
+//
+
+import Foundation
+
+class AppState {
+
+    static let shared = AppState()
+
+    private init() {}
+}
