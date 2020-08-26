8470ffebf93c0038d65e9375fe72e920ffc03aa1
nuomi1 <nuomi1@qq.com>
2020-08-26 20:50:59 +0800

    feat: PlatformViewController

---
+++ Example/NBus/View/PlatformViewController.swift
@@ -0,0 +1,11 @@
+//
+//  PlatformViewController.swift
+//  BusMock
+//
+//  Created by nuomi1 on 2020/8/26.
+//  Copyright © 2020 nuomi1. All rights reserved.
+//
+
+import UIKit
+
+class PlatformViewController: UIViewController {}
