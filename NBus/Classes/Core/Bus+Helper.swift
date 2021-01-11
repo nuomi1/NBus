f413ae8de42a81b169d913864c53435325d24bf7
nuomi1 <nuomi1@qq.com>
2021-01-11 23:36:13 +0800

    feat: BusWrapper with Bundle

--- NBus/Classes/Core/Bus+Helper.swift
+++ NBus/Classes/Core/Bus+Helper.swift
@@ -22,3 +22,33 @@ extension BusWrapper where Base == [Bus.OauthInfoKey: String?] {
         }
     }
 }
+
+extension NSObject: BusCompatible {}
+
+extension BusWrapper where Base: Bundle {
+
+    private func value<T>(forKeys keys: [String]) -> T? {
+        let infos = [
+            base.localizedInfoDictionary ?? [:],
+            base.infoDictionary ?? [:],
+        ]
+
+        for key in keys {
+            for info in infos {
+                if let value = info[key] as? T {
+                    return value
+                }
+            }
+        }
+
+        return nil
+    }
+
+    public var identifier: String? {
+        value(forKeys: ["CFBundleIdentifier"])
+    }
+
+    public var displayName: String? {
+        value(forKeys: ["CFBundleDisplayName", "CFBundleName"])
+    }
+}
