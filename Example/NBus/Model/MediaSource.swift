bbb7c3df772d7dbfa72b1022c0e454a51112c153
nuomi1 <nuomi1@qq.com>
2021-02-07 23:29:57 +0800

    fix: lint

--- Example/NBus/Model/MediaSource.swift
+++ Example/NBus/Model/MediaSource.swift
@@ -11,15 +11,11 @@ import UIKit
 
 enum MediaSource {
 
-    // swiftlint:disable line_length
-
     static let text: MessageType = {
         Messages.text(
             text: defaultText
         )
     }()
-
-    // swiftlint:enable line_length
 
     static let image: MessageType = {
         let data = defaultJPEG.jpegData(compressionQuality: 1)!
@@ -124,12 +120,16 @@ enum MediaSource {
 
 extension MediaSource {
 
+    // swiftlint:disable line_length
+
     private static var defaultText: String {
         // https://suulnnka.github.io/BullshitGenerator/?%E4%B8%BB%E9%A2%98=BUS&%E9%9A%8F%E6%9C%BA%E7%A7%8D%E5%AD%90=1038191506
         let text = "那么， 带着这些问题，我们来审视一下BUS。 可是，即使是这样，BUS的出现仍然代表了一定的意义。 BUS的发生，到底需要如何做到，不BUS的发生，又会如何产生。 生活中，若BUS出现了，我们就不得不考虑它出现了的事实。 经过上述讨论， 那么， 既然如何， 现在，解决BUS的问题，是非常非常重要的。 所以， 我们都知道，只要有意义，那么就必须慎重考虑。 这种事实对本人来说意义重大，相信对这个世界也是有一定意义的。 康德曾经提到过，既然我已经踏上这条道路，那么，任何东西都不应妨碍我沿着这条路走下去。这似乎解答了我的疑惑。"
 
         return text
     }
+
+    // swiftlint:enable line_length
 
     private static var defaultJPEG: UIImage {
         // https://unsplash.com/photos/CEubYUySRo4
