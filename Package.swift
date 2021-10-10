910782a97a8415db5c07ac1303045cd12857803e
nuomi1 <nuomi1@qq.com>
2021-10-10 11:19:08 +0800

    feat: add Package.swift

---
+++ Package.swift
@@ -0,0 +1,63 @@
+// swift-tools-version:5.2
+
+import PackageDescription
+
+let package = Package(
+    name: "NBus",
+    platforms: [
+        .iOS(.v10),
+    ],
+    products: [
+        .library(
+            name: "NBusHandlers",
+            targets: [
+                "NBusQQHandler",
+                "NBusWechatHandler",
+                "NBusWeiboHandler",
+                "NBusSystemHandler",
+            ]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "NBusCore",
+            path: "NBus/Classes/Core"
+        ),
+        .target(
+            name: "NBusQQHandler",
+            dependencies: ["NBusCore"],
+            path: "NBus/Classes/Handler",
+            sources: [
+                "QQHandler.swift",
+            ]
+        ),
+        .target(
+            name: "NBusWechatHandler",
+            dependencies: ["NBusCore"],
+            path: "NBus/Classes/Handler",
+            sources: [
+                "WechatHandler.swift",
+            ]
+        ),
+        .target(
+            name: "NBusWeiboHandler",
+            dependencies: ["NBusCore"],
+            path: "NBus/Classes/Handler",
+            sources: [
+                "WeiboHandler.swift",
+            ]
+        ),
+        .target(
+            name: "NBusSystemHandler",
+            dependencies: ["NBusCore"],
+            path: "NBus/Classes/Handler",
+            sources: [
+                "SystemHandler.swift",
+            ]
+        ),
+        .testTarget(
+            name: "NBusTests",
+            dependencies: ["NBusCore"]
+        ),
+    ]
+)
