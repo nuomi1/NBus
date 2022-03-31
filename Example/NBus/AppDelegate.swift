e63954997b8581469c5c090716102e358fac9cda
nuomi1 <nuomi1@qq.com>
2022-04-01 00:27:23 +0800

    refactor: observeSystem

--- Example/NBus/AppDelegate.swift
+++ Example/NBus/AppDelegate.swift
@@ -70,12 +70,10 @@ extension AppDelegate {
     // swiftlint:disable function_body_length
 
     private func pasteboardItems() -> Observable<[[String]]> {
-        NotificationCenter.default.rx
-            .notification(UIPasteboard.changedNotification)
-            .map { _ -> [[String]] in
-                let items = UIPasteboard.general.items
-
-                return items.enumerated().map { index, item -> [String] in
+        UIPasteboard.general.rx
+            .items()
+            .map { items -> [[String]] in
+                items.enumerated().map { index, item -> [String] in
                     item.map { key, value -> String in
                         let identity: String
                         let index = "(\(index + 1)/\(items.count))"
@@ -126,30 +124,6 @@ extension AppDelegate {
     }
 
     // swiftlint:enable function_body_length
-
-    private func canOpenURL() -> Observable<URL> {
-        UIApplication.shared.rx
-            .methodInvoked(#selector(UIApplication.canOpenURL(_:)))
-            .compactMap { args in
-                args[0] as? URL
-            }
-    }
-
-    private func openURL() -> Observable<URL> {
-        let oldURL = UIApplication.shared.rx
-            .methodInvoked(#selector(UIApplication.openURL(_:)))
-            .compactMap { args in
-                args[0] as? URL
-            }
-
-        let newURL = UIApplication.shared.rx
-            .methodInvoked(#selector(UIApplication.open(_:options:completionHandler:)))
-            .compactMap { args in
-                args[0] as? URL
-            }
-
-        return Observable.merge([oldURL, newURL])
-    }
 }
 
 extension AppDelegate {
@@ -161,13 +135,15 @@ extension AppDelegate {
             })
             .disposed(by: disposeBag)
 
-        canOpenURL()
+        UIApplication.shared.rx
+            .canOpenURL()
             .bind(onNext: { url in
                 logger.debug("\(url)")
             })
             .disposed(by: disposeBag)
 
-        openURL()
+        UIApplication.shared.rx
+            .openURL()
             .bind(onNext: { url in
                 logger.debug("\(url)")
             })
---
+++ Example/NBus/Model/Helper.swift
@@ -0,0 +1,44 @@
+//
+//  Helper.swift
+//  NBus
+//
+//  Created by nuomi1 on 2022/4/1.
+//  Copyright © 2022 nuomi1. All rights reserved.
+//
+
+import Foundation
+import RxCocoa
+import RxSwift
+
+extension Reactive where Base: UIApplication {
+
+    func canOpenURL() -> Observable<URL> {
+        methodInvoked(#selector(UIApplication.canOpenURL(_:)))
+            .compactMap { args in
+                args[0] as? URL
+            }
+    }
+
+    func openURL() -> Observable<URL> {
+        let oldURL = methodInvoked(#selector(UIApplication.openURL(_:)))
+            .compactMap { args in
+                args[0] as? URL
+            }
+
+        let newURL = methodInvoked(#selector(UIApplication.open(_:options:completionHandler:)))
+            .compactMap { args in
+                args[0] as? URL
+            }
+
+        return Observable.merge([oldURL, newURL])
+    }
+}
+
+extension Reactive where Base: UIPasteboard {
+
+    func items() -> Observable<[[String: Any]]> {
+        NotificationCenter.default.rx
+            .notification(UIPasteboard.changedNotification)
+            .map { _ in base.items }
+    }
+}
