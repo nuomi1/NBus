7e3bb67d3f9dfa480ee9f8197c8d7b7887603b4e
nuomi1 <nuomi1@qq.com>
2022-04-05 14:13:49 +0800

    feat: ShareTestCase

--- Tests/NBusTests/Base/HandlerBaseTests.swift
+++ Tests/NBusTests/Base/HandlerBaseTests.swift
@@ -52,6 +52,9 @@ class HandlerBaseTests: XCTestCase {
     static var disposeBag = DisposeBag()
     var disposeBag = DisposeBag()
 
+    let ulExpectation = XCTestExpectation(description: "UniversalLink")
+    let pbExpectation = XCTestExpectation(description: "Pasteboard")
+
     override class func setUp() {
         super.setUp()
 
@@ -117,5 +120,97 @@ extension HandlerBaseTests {
         super.tearDown()
 
         disposeBag = DisposeBag()
+    }
+}
+
+// MARK: - Share
+
+extension ShareTestCase {
+
+    func test_share(_ message: MessageType, _ endpoint: Endpoint) {
+        UIApplication.shared.rx
+            .openURL()
+            .bind(onNext: { [unowned self] url in
+                self.test_share(url: url, message, endpoint)
+            })
+            .disposed(by: disposeBag)
+
+        UIPasteboard.general.rx
+            .items()
+            .filter { !$0.isEmpty }
+            .bind(onNext: { [unowned self] items in
+                self.test_share(items: items, message, endpoint)
+            })
+            .disposed(by: disposeBag)
+
+        Bus.shared.share(
+            message: message,
+            to: endpoint,
+            completionHandler: { [unowned self] result in
+                self.test_share(result: result, message, endpoint)
+            }
+        )
+
+        wait(for: [ulExpectation, pbExpectation], timeout: 5)
+    }
+}
+
+// MARK: - Share - URL
+
+extension ShareURLTestCase {
+
+    func test_share(url: URL, _ message: MessageType, _ endpoint: Endpoint) {
+        let urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: false)!
+        var queryItems = urlComponents.queryItems ?? []
+
+        logger.debug("\(URLComponents.self), start, \(queryItems.map(\.name).sorted())")
+
+        // General - UniversalLink
+
+        test_general_ul(scheme: urlComponents.scheme!)
+        test_general_ul(host: urlComponents.host!)
+        test_general_ul(queryItems: &queryItems)
+
+        // Share - UniversalLink
+
+        test_share_ul(path: urlComponents.path)
+        test_share_ul(queryItems: &queryItems)
+
+        // Share - MediaMessage - UniversalLink
+
+        test_share_media_ul(queryItems: &queryItems, message, endpoint)
+
+        // Share - Message - UniversalLink
+
+        test_share_message_ul(queryItems: &queryItems, message, endpoint)
+
+        logger.debug("\(URLComponents.self), end, \(queryItems.map(\.name).sorted())")
+
+        XCTAssertTrue(queryItems.isEmpty)
+
+        ulExpectation.fulfill()
+    }
+}
+
+// MARK: - Share - Completion
+
+extension ShareCompletionTestCase {
+
+    func test_share(result: Result<Void, Bus.Error>, _ message: MessageType, _ endpoint: Endpoint) {
+        switch result {
+        case .success:
+            XCTAssertTrue(true)
+        case let .failure(error):
+            logger.error("\(error)")
+
+            if test_share_avoid_error(error, message, endpoint) {
+                XCTAssertTrue(true)
+
+                ulExpectation.fulfill()
+                pbExpectation.fulfill()
+            } else {
+                XCTAssertTrue(false)
+            }
+        }
     }
 }
