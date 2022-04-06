c81c56f9a20bcc4f03eab2444f82e9194cb01cdc
nuomi1 <nuomi1@qq.com>
2022-04-06 22:33:35 +0800

    feat: OauthTestCase implementation

--- Tests/NBusTests/Base/HandlerBaseTests.swift
+++ Tests/NBusTests/Base/HandlerBaseTests.swift
@@ -298,3 +298,112 @@ extension ShareCompletionTestCase {
         }
     }
 }
+
+// MARK: - Oauth
+
+extension OauthTestCase {
+
+    func test_oauth(_ platform: Platform) {
+        UIApplication.shared.rx
+            .openURL()
+            .bind(onNext: { [unowned self] url in
+                self.test_oauth(url: url, platform)
+            })
+            .disposed(by: disposeBag)
+
+        UIPasteboard.general.rx
+            .items()
+            .filter { !$0.isEmpty }
+            .bind(onNext: { [unowned self] items in
+                self.test_oauth(items: items, platform)
+            })
+            .disposed(by: disposeBag)
+
+        Bus.shared.oauth(
+            with: platform,
+            completionHandler: { [unowned self] result in
+                self.test_oauth(result: result, platform)
+            }
+        )
+
+        wait(for: [ulExpectation, pbExpectation], timeout: 5)
+    }
+}
+
+extension OauthURLTestCase {
+
+    func test_oauth(url: URL, _ platform: Platform) {
+        let urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: false)!
+        var queryItems = urlComponents.queryItems ?? []
+
+        logger.debug("\(URLComponents.self), start, \(queryItems.map(\.name).sorted())")
+
+        // General - UniversalLink
+
+        test_general_ul(scheme: urlComponents.scheme!)
+        test_general_ul(host: urlComponents.host!)
+        test_general_ul(queryItems: &queryItems)
+
+        // Oauth - UniversalLink
+
+        test_oauth_ul(path: urlComponents.path)
+        test_oauth_ul(queryItems: &queryItems, platform)
+
+        logger.debug("\(URLComponents.self), end, \(queryItems.map(\.name).sorted())")
+
+        XCTAssertTrue(queryItems.isEmpty)
+
+        ulExpectation.fulfill()
+    }
+}
+
+extension OauthPasteboardTestCase {
+
+    func test_oauth(items: [[String: Any]], _ platform: Platform) {
+        var items = items as! [[String: Data]]
+
+        logger.debug("\(UIPasteboard.self), start, \(items.map { $0.keys.sorted() })")
+
+        test_oauth_major_pb(dictionary: test_extract_major_pb(items: &items), platform)
+
+        test_extra_pb(items: &items)
+
+        logger.debug("\(UIPasteboard.self), end, \(items.map { $0.keys.sorted() })")
+
+        XCTAssertTrue(items.isEmpty)
+
+        pbExpectation.fulfill()
+    }
+
+    func test_oauth_major_pb(dictionary: [String: Any], _ platform: Platform) {
+        var dictionary = dictionary
+
+        logger.debug("\(UIPasteboard.self), start, \(dictionary.keys.sorted())")
+
+        // General - Pasteboard
+
+        test_general_pb(dictionary: &dictionary)
+
+        // Oauth - Platform - Pasteboard
+
+        test_oauth_pb(dictionary: &dictionary, platform)
+
+        logger.debug("\(UIPasteboard.self), end, \(dictionary.keys.sorted())")
+
+        XCTAssertTrue(dictionary.isEmpty)
+    }
+}
+
+// MARK: - Oauth - Completion
+
+extension OauthCompletionTestCase {
+
+    func test_oauth(result: Result<[Bus.OauthInfoKey: String], Bus.Error>, _ platform: Platform) {
+        switch result {
+        case .success:
+            XCTAssertTrue(true)
+        case .failure:
+            XCTAssertTrue(false)
+        }
+    }
+}
