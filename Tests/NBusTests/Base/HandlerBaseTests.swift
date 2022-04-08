e6947cd6b7c936c4ee30ae9b9af4f6879d059bab
nuomi1 <nuomi1@qq.com>
2022-04-08 20:27:16 +0800

    feat: LaunchTestCase implementation

--- Tests/NBusTests/Base/HandlerBaseTests.swift
+++ Tests/NBusTests/Base/HandlerBaseTests.swift
@@ -418,3 +418,115 @@ extension OauthCompletionTestCase {
         }
     }
 }
+
+// MARK: - Launch
+
+extension LaunchTestCase {
+
+    func test_launch(_ platform: Platform, _ program: MiniProgramMessage) {
+        UIApplication.shared.rx
+            .openURL()
+            .bind(onNext: { [unowned self] url in
+                self.test_launch(url: url, platform, program)
+            })
+            .disposed(by: disposeBag)
+
+        UIPasteboard.general.rx
+            .items()
+            .filter { !$0.isEmpty }
+            .bind(onNext: { [unowned self] items in
+                self.test_launch(items: items, platform, program)
+            })
+            .disposed(by: disposeBag)
+
+        Bus.shared.launch(
+            program: program,
+            with: platform,
+            completionHandler: { [unowned self] result in
+                self.test_launch(result: result, platform, program)
+            }
+        )
+    }
+}
+
+// MARK: - Launch - URL
+
+extension LaunchURLTestCase {
+
+    func test_launch(url: URL, _ platform: Platform, _ program: MiniProgramMessage) {
+        let urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: false)!
+        var queryItems = urlComponents.queryItems ?? []
+
+        logger.debug("\(URLComponents.self), start, \(queryItems.map(\.name).sorted())")
+
+        // General - UniversalLink
+
+        test_general_ul(scheme: urlComponents.scheme!)
+        test_general_ul(host: urlComponents.host!)
+        test_general_ul(queryItems: &queryItems)
+
+        // Launch - UniversalLink
+
+        test_launch_ul(path: urlComponents.path)
+        test_launch_ul(queryItems: &queryItems, platform, program)
+
+        logger.debug("\(URLComponents.self), end, \(queryItems.map(\.name).sorted())")
+
+        XCTAssertTrue(queryItems.isEmpty)
+
+        ulExpectation.fulfill()
+    }
+}
+
+// MARK: - Launch - Pasteboard
+
+extension LaunchPasteboardTestCase {
+
+    func test_launch(items: [[String: Any]], _ platform: Platform, _ program: MiniProgramMessage) {
+        var items = items as! [[String: Data]]
+
+        logger.debug("\(UIPasteboard.self), start, \(items.map { $0.keys.sorted() })")
+
+        test_launch_major_pb(dictionary: test_extract_major_pb(items: &items), platform, program)
+
+        test_extra_pb(items: &items)
+
+        logger.debug("\(UIPasteboard.self), end, \(items.map { $0.keys.sorted() })")
+
+        XCTAssertTrue(items.isEmpty)
+
+        pbExpectation.fulfill()
+    }
+
+    func test_launch_major_pb(dictionary: [String: Any], _ platform: Platform, _ program: MiniProgramMessage) {
+        var dictionary = dictionary
+
+        logger.debug("\(UIPasteboard.self), start, \(dictionary.keys.sorted())")
+
+        // General - Pasteboard
+
+        test_general_pb(dictionary: &dictionary)
+
+        // Launch - Program - Pasteboard
+
+        test_launch_pb(dictionary: &dictionary, platform, program)
+
+        logger.debug("\(UIPasteboard.self), end, \(dictionary.keys.sorted())")
+
+        XCTAssertTrue(dictionary.isEmpty)
+    }
+}
+
+// MARK: - Launch - Completion
+
+extension LaunchCompletionTestCase {
+
+    func test_launch(result: Result<Void, Bus.Error>, _ platform: Platform, _ program: MiniProgramMessage) {
+        switch result {
+        case .success:
+            XCTAssertTrue(true)
+        case .failure:
+            XCTAssertTrue(false)
+        }
+    }
+}
